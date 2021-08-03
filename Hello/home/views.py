2ecaed778cd14fdd77ab6f75ede9a7c446624844
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-08-03 09:36:37 +0530

    code

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -5,3 +5,7 @@ def index(request):
     return HttpResponse("This is home page")
 def about(request):
     return HttpResponse("This is about page")
+def service(request):
+    return HttpResponse("This is service page")
+def contact(request):
+    return HttpResponse("This is contact page")
