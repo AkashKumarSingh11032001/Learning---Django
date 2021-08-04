afdb9cc8279d2356d77b957a6d5c15a2e4caadfd
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-08-04 15:39:04 +0530

    code

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -10,8 +10,15 @@ def index(request):
     return render(request,'index.html',context)
     
 def about(request):
-    return HttpResponse("This is about page")
+    # return HttpResponse("This is about page")
+    return render(request,'about.html')
+
+
 def service(request):
-    return HttpResponse("This is service page")
+    # return HttpResponse("This is service page")
+    return render(request,'service.html')
+
+
 def contact(request):
-    return HttpResponse("This is contact page")
+    # return HttpResponse("This is contact page")
+    return render(request,'contact.html')
