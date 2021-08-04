80da9149a72d9d329b241cd3c82725f0e7007b4c
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-08-04 11:25:01 +0530

    code

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is home page")
+    context = {  # sending user value to html page ***
+        'var1':'Akash',
+        'var2':'Suwar'
+    }
+    # return HttpResponse("This is home page")
+    return render(request,'main.html',context)
 def about(request):
     return HttpResponse("This is about page")
 def service(request):
