1eb2410eb68d74d3ac1df9fa6d8451705e11e1f6
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-08-04 11:35:30 +0530

    code

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -7,7 +7,7 @@ def index(request):
         'var2':'Suwar'
     }
     # return HttpResponse("This is home page")
-    return render(request,'main.html',context)
+    return render(request,'index.html',context)
 def about(request):
     return HttpResponse("This is about page")
 def service(request):
