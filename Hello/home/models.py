dcc3528ec4e0721d69179d78fe95c4a69e323166
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-08-05 09:33:46 +0530

    code

--- Hello/home/models.py
+++ Hello/home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name = models.CharField(max_length=122)
+    name = models.CharField(max_length=255)
     email = models.CharField(max_length=122)
     phone = models.CharField(max_length=122)
     desc = models.TextField(blank=True)
